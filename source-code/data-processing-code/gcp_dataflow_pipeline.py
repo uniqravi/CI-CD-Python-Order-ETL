21715532157fe813f409813a74667aaa6f0a31c4
uniqravi <ravi.khandelwal23@gmail.com>
2022-01-28 14:29:49 +0530

    order pipeline build

--- source-code/data-processing-code/gcp_dataflow_pipeline.py
+++ source-code/data-processing-code/gcp_dataflow_pipeline.py
@@ -5,6 +5,7 @@ import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 import datetime
 
+
 class OlistDatasetOptions(PipelineOptions):
     @classmethod
     def _add_argparse_args(cls, parser):
@@ -14,18 +15,14 @@ class OlistDatasetOptions(PipelineOptions):
             '--input',
             default='gs://dataflow-samples/shakespeare/kinglear.txt',
             help='Path of the file to read from')
-        parser.add_argument(
-            '--project',
-            default='gcp-learning-333002',
-            help='project id')
 
 
 if __name__ == '__main__':
-    #datetime.strptime("21/11/06 16:30", "%d/%m/%y %H:%M")
+    # datetime.strptime("21/11/06 16:30", "%d/%m/%y %H:%M")
     logging.getLogger().setLevel(logging.INFO)
     # Create the Pipeline with remaining arguments.
     beam_options = PipelineOptions()
-    #pipeline = beam.Pipeline(options=beam_options)
+    # pipeline = beam.Pipeline(options=beam_options)
 
     with beam.Pipeline(options=beam_options) as p:
         olist_dataset_options = beam_options.view_as(OlistDatasetOptions)
@@ -37,11 +34,15 @@ if __name__ == '__main__':
          | 'Split' >> beam.Map(lambda x: x.split(','))
          | 'Filter Blank Row' >> beam.Filter(lambda x: x[0] != '' and x[1] != '')
          | 'Making product Dict Map' >> beam.Map(
-                    lambda x: {'order_id': x[0], 'customer_id': x[1],'order_status':int(x[2]),'order_purchase_timestamp':datetime.strptime(x[3], "%y-%m-%d %H:%M:%S"),
-                               'order_approved_at':datetime.strptime(x[4], "%y-%m-%d %H:%M:%S"),'order_delivered_carrier_date':datetime.strptime(x[5], "%y-%m-%d %H:%M:%S"),
-                               'order_delivered_customer_date':datetime.strptime(x[6], "%y-%m-%d %H:%M:%S"),'order_estimated_delivery_date':datetime.strptime(x[7], "%y-%m-%d %H:%M:%S")
+                    lambda x: {'order_id': x[0], 'customer_id': x[1], 'order_status': int(x[2]),
+                               'order_purchase_timestamp': datetime.strptime(x[3], "%y-%m-%d %H:%M:%S"),
+                               'order_approved_at': datetime.strptime(x[4], "%y-%m-%d %H:%M:%S"),
+                               'order_delivered_carrier_date': datetime.strptime(x[5], "%y-%m-%d %H:%M:%S"),
+                               'order_delivered_customer_date': datetime.strptime(x[6], "%y-%m-%d %H:%M:%S"),
+                               'order_estimated_delivery_date': datetime.strptime(x[7], "%y-%m-%d %H:%M:%S")
                                })
-         | 'Write ProductCat BigQuery' >> beam.io.WriteToBigQuery(table=str(olist_dataset_options.project)+':olist_demo_dataset.olist_order',
-                                                                  schema=SCHEMA,
-                                                                  write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND)
+         | 'Write ProductCat BigQuery' >> beam.io.WriteToBigQuery(
+                    table=str(olist_dataset_options.project) + ':olist_demo_dataset.olist_order',
+                    schema=SCHEMA,
+                    write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND)
          )
